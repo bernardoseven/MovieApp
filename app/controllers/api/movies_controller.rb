86857fd348aef0d4d91782f93b78d23f5c458051
bernardoseven <bernardoseven@gmail.com>
2016-01-12 13:04:21 +0000

    implemented show action api

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -2,4 +2,9 @@ class Api::MoviesController < ApplicationController
     def index
         render json: Movie.all
     end
+    
+    def show
+        list = Movie.find(params[:id])
+        render json: list
+    end
 end
